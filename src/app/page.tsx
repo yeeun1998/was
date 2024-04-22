36da294ca4e2a33e16e98340114c7ced0dec5e91
yeeun1998 <hye0403@naver.com>
2024-04-22 11:12:13 +0900

    second commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import styles from '../styles/Home.module.css';
+
+function HomePage() {
+  return (
+    <div className={styles.container}>
+      한예은
+    </div>
+  );
+}
+
+export default HomePage;
